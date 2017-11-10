92540e794c17e609a05617eac1f5a8ad3e28a308
samuelsoc <samuelrg.sna@gmail.com>
2017-11-10 16:13:01 -0300

    nuevos comentarios

---
+++ recolectStats.R
@@ -0,0 +1,69 @@
+#extraer datos desde API de Junar, estaditicas de los portales
+# endpoint stats ejemplo: 
+#http://pilar.cloudapi.junar.com/api/v2/stats/?auth_key=ac0d7fc2804b535637ae44aa5f0bc57c674d3353
+
+#url doc api: https://junar.github.io/docs/es/_sections/07-estadisticas.html
+
+#Librerias
+
+library(httr)
+library(jsonlite)
+library(dplyr)
+
+###Autenticación
+
+web <- "http://datosabiertos.pilar.gov.ar" # url home del cliente
+cliente <- "http://pilar.cloudapi.junar.com" # url api cliente
+key <- "auth_key=ac0d7fc2804b535637ae44aa5f0bc57c674d3353" # auth_key
+url <- "/api/v2/stats/?" # sección url api
+req <- fromJSON(paste0(cliente,url, key))
+
+##Recolección datastream
+stats_ds <- req$datastreams$stats$resources #datastream estadisticas
+total_ds <- req$datastreams$total # solo es un número
+
+##Recolección visualization
+stats_vz <- req$visualizations$stats$resources #visualization estadisticas
+total_vz <- req$visualizations$total # solo es un número
+
+##Recolección dashboard
+stats_db <- req$dashboards$stats$resources #colecciones estadisticas
+total_db <- req$dashboards$total # solo es un número
+
+##Limpieza/seleccion columnas.
+# Se agrego columna nueva URL con la direccion completa del recurso
+
+# datstream
+  ds_m <-mutate(stats_ds, URL = (paste0( web,stats_ds$landingPage)) ) %>% 
+  select('Cantidad Visitas' = count,
+         'Nombre Vista' =title,
+         'Descripción' = description,
+         'URL Vista' = URL,
+         'Categoria' = category,
+         'GUID' = guid)
+  
+# Visualizacion
+  
+  vz_m <-mutate(stats_vz, URL = (paste0( web,stats_vz$landingPage)) ) %>% 
+    select('Cantidad Visitas' = count,
+           'Nombre Vista' =title,
+           'Descripción' = description,
+           'URL Vista' = URL,
+           'Categoria' = category,
+           'GUID' = guid)
+  
+# Colecciones
+  
+  db_m <-mutate(stats_db, URL = (paste0( web,stats_db$landingPage)) ) %>% 
+    select('Cantidad Visitas' = count,
+           'Nombre Vista' =title,
+           'Descripción' = description,
+           'URL Vista' = URL,
+           'Categoria' = category,
+           'GUID' = guid)
+  
+  
+             
+
+
+
