1a6ba34b0c5e4d9704e5fba3ec0f65d4a3b73f3f
samuelsoc <samuelrg.sna@gmail.com>
2017-12-17 15:07:26 -0300

    funciones con fechas

--- getstats.R
+++ getstats.R
@@ -1,13 +1,25 @@
 getstats.R
 
+library(httr)
+library(jsonlite)
+library(dplyr)
+
 #Stats from resources
 
 
 #base_url = http://pilar.cloudapi.junar.com/api/v2/stats/
 #api_key = ac0d7fc2804b535637ae44aa5f0bc57c674d3353
+#from = 1/1/2017
+#to = 30/11/2017
+#month = month
 
  base_url = "http://pilar.cloudapi.junar.com/api/v2/stats/"
  api_key = "ac0d7fc2804b535637ae44aa5f0bc57c674d3353"
+
+ from = "1/1/2017"
+ to = "30/11/2017"
+ month = "month"
+
     
 
   
@@ -69,3 +81,53 @@ getstats.R
      })
  }
   
+
+#HITS BY DATE
+
+  #Hits to datastream_date
+   get_statsDS_Date <- function(base_url, api_key, from, to){
+     if (missing(base_url)) {
+       warning("Please add a valid base URL")
+     } else if (missing(api_key)) {
+       warning("Please add a valid API key for the base URL you are trying to access")
+     } else if (missing (from)) {
+       warning("Please add a valid Date format dd/mm/yyyy")
+     } else if (missing(to)) {
+       warning("Please add a valid Date format dd/mm/yyyy")
+     }
+     try({
+       r <- GET(paste(base_url, "?auth_key=", api_key, "&from=", from, "&to=", to, sep=""), accept_json())
+       dataset <- fromJSON(content(r, "text"))
+       dataset <- dataset$datastream$stats$resources
+       df <- as.data.frame(dataset)
+       colnames(df) <- dataset[1,]
+       df <- df [-1,]   
+       return(dataset)
+     })
+   }
+  
+  get_statsDS_Date(base_url, api_key, from, to)
+  
+  
+  #Hits GroupByMonth
+  
+  get_statsDS_Month <- function(base_url, api_key, month){
+    if (missing(base_url)) {
+      warning("Please add a valid base URL")
+    } else if (missing(api_key)) {
+      warning("Please add a valid API key for the base URL you are trying to access")
+    } 
+    try({
+      r <- GET(paste(base_url, "?auth_key=", api_key, "&group_by=", month, sep=""), accept_json())
+      dataset <- fromJSON(content(r, "text"))
+      dataset <- dataset$datastreams$stats$articles_over_time
+      df <- as.data.frame(dataset)
+      colnames(df) <- dataset[1,]
+      df <- df [-1,]   
+      return(dataset)
+    })
+  }
+
+  get_statsDS_Month(base_url,api_key, month)
+
+
